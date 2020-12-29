68062a5bb1b2a8fda79d7fba95cdf188193f5d19
nb181916 <NicholasN@Nedbank.co.za>
2020-12-29 08:02:44 +0200

    Added IApiClient Interface

---
+++ Munharaunda/Munharaunda.Domain/Contracts/IApiClient.cs
@@ -0,0 +1,55 @@
+﻿using Munharaunda.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Munharaunda.Domain.Contracts
+{
+    public interface IApiClient
+    {
+        Task<ResponseModel<Funeral>> GetAllFunerals();
+        Task<ResponseModel<Funeral>> GetFuneralById(int id);
+        Task<ResponseModel<Funeral>> UpdateFuneral(int id, Funeral funeral);
+        Task<ResponseModel<Funeral>> CreateFuneral(Funeral funeral);
+        Task<ResponseModel<Funeral>> DeleteFuneralById(int id);
+
+        Task<ResponseModel<IdentityTypes>> GetAllIdentityTypes();
+        Task<ResponseModel<IdentityTypes>> GetIdentityTypeById(int id);
+        Task<ResponseModel<IdentityTypes>> CreateIdentityType(IdentityTypes identityType);
+        Task<ResponseModel<IdentityTypes>> UpdateIdentityType(int id, IdentityTypes identityType);
+        Task<ResponseModel<IdentityTypes>> DeleteIdentityType(int id);
+
+        Task<ResponseModel<Profile>> GetAllProfiles();
+        Task<ResponseModel<Profile>> GetProfileById(int id);
+        Task<ResponseModel<Profile>> CreateProfile(Profile Profile);
+        Task<ResponseModel<Profile>> UpdateProfile(int id, Profile Profile);
+        Task<ResponseModel<Profile>> DeleteProfile(int id);
+
+        Task<ResponseModel<ProfileTypes>> GetAllProfileTypes();
+        Task<ResponseModel<ProfileTypes>> GetProfileTypeById(int id);
+        Task<ResponseModel<ProfileTypes>> CreateProfileType(ProfileTypes profileType);
+        Task<ResponseModel<ProfileTypes>> UpdateProfileType(int id, ProfileTypes profileType);
+        Task<ResponseModel<ProfileTypes>> DeleteProfileType(int id);
+
+        Task<ResponseModel<Statuses>> GetAllStatuses();
+        Task<ResponseModel<Statuses>> GetStatusById(int id);
+        Task<ResponseModel<Statuses>> CreateStatuses(Statuses status);
+        Task<ResponseModel<Statuses>> UpdateStatus(int id, Statuses status);
+        Task<ResponseModel<Statuses>> DeleteStatus(int id);
+
+        Task<ResponseModel<TransactionCodes>> GetAllTransactionCodes();
+        Task<ResponseModel<TransactionCodes>> GetTransactionCodeById(int id);
+        Task<ResponseModel<TransactionCodes>> CreateTransactionCode(TransactionCodes transactionCode);
+        Task<ResponseModel<TransactionCodes>> UpdateTransactionCode(int id, TransactionCodes transactionCode);
+        Task<ResponseModel<TransactionCodes>> DeleteTransactionCode(int id);
+
+        Task<ResponseModel<Transactions>> GetAllTransactions();
+        Task<ResponseModel<Transactions>> GetTransactionById(int id);
+        Task<ResponseModel<Transactions>> CreateTransactions(Transactions transaction);
+        Task<ResponseModel<Transactions>> UpdateTransactions(int id, Transactions transaction);
+        Task<ResponseModel<Transactions>> DeleteTransactions(int id);
+
+
+    }
+}
---
+++ Munharaunda/Munharaunda.Web/WebServices/ApiClient.cs
@@ -0,0 +1,194 @@
+﻿using Munharaunda.Domain.Contracts;
+using Munharaunda.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Munharaunda.Web.WebServices
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public Task<ResponseModel<Funeral>> CreateFuneral(Funeral funeral)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<IdentityTypes>> CreateIdentityType(IdentityTypes identityType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Profile>> CreateProfile(Profile Profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<ProfileTypes>> CreateProfileType(ProfileTypes profileType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Statuses>> CreateStatuses(Statuses status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<TransactionCodes>> CreateTransactionCode(TransactionCodes transactionCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Transactions>> CreateTransactions(Transactions transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Funeral>> DeleteFuneralById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<IdentityTypes>> DeleteIdentityType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Profile>> DeleteProfile(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<ProfileTypes>> DeleteProfileType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Statuses>> DeleteStatus(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<TransactionCodes>> DeleteTransactionCode(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Transactions>> DeleteTransactions(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Funeral>> GetAllFunerals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<IdentityTypes>> GetAllIdentityTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Profile>> GetAllProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<ProfileTypes>> GetAllProfileTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Statuses>> GetAllStatuses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<TransactionCodes>> GetAllTransactionCodes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Transactions>> GetAllTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Funeral>> GetFuneralById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<IdentityTypes>> GetIdentityTypeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Profile>> GetProfileById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<ProfileTypes>> GetProfileTypeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Statuses>> GetStatusById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Transactions>> GetTransactionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<TransactionCodes>> GetTransactionCodeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Funeral>> UpdateFuneral(int id, Funeral funeral)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<IdentityTypes>> UpdateIdentityType(int id, IdentityTypes identityType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Profile>> UpdateProfile(int id, Profile Profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<ProfileTypes>> UpdateProfileType(int id, ProfileTypes profileType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Statuses>> UpdateStatus(int id, Statuses status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<TransactionCodes>> UpdateTransactionCode(int id, TransactionCodes transactionCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<Transactions>> UpdateTransactions(int id, Transactions transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
